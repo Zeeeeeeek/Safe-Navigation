8dd607728810a54114f36af2d6d25c0d77013ca3
Enrico <enricoulissi01@gmail.com>
2025-01-10 00:24:21 +0100

    add canvas for mail

--- Assets/Scripts/IslandCollectible.cs
+++ Assets/Scripts/IslandCollectible.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,12 +9,18 @@ public class IslandCollectible : MonoBehaviour
 {
     public string islandId;
     public IslandEnterEvent onPlayerEnter;
+    [DllImport("__Internal")]
+    private static extern void SetScore(int score);
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-        // Lancia l'evento con l'ID dell'isola come parametro
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            SetScore(1000);
+        #else
+            Debug.Log("Player entered island");
+        #endif
         onPlayerEnter?.Invoke(islandId);
     }
 }
---
+++ Assets/Scripts/plugin.jslib
@@ -0,0 +1,9 @@
+﻿mergeInto(LibraryManager.library, {
+
+  SetScore: function (score) {
+    console.log("Setting score: ", score);
+    console.log("Score type ", typeof score);
+    const progressData = {score: score};
+    window.parent.mod_webgl_plugin.trackGameProgress(progressData);
+  },
+});
