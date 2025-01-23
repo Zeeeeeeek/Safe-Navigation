ae283287ce55744e76ea756b1e92bebb3713202d
Enrico <enricoulissi01@gmail.com>
2025-01-23 12:21:08 +0100

    add health

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public int health = 10;
+    public TextMeshProUGUI healthText;
+
+    public void Start()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        healthText.text = $"{health}";
+    }
+
+    private void DecreaseHealth()
+    {
+        if (health <= 0) return;
+        health--;
+        UpdateText();
+    }
+
+    public void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        DecreaseHealth();
+    }
+}
